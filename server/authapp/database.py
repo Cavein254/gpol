1eaf1bfa2aea511ce0381d691437f9aca1be3c8f
johnmuthua <caveomuthama@gmail.com>
2023-08-01 09:36:01 +0300

    Add alembic

--- server/authapp/database.py
+++ server/authapp/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
 
-DATABASE_URL = "postgresql+psycopg2://postgres:mypassword@localhost/blog"
-engine = create_engine(DATABASE_URL)
+load_dotenv()
+
+database_url = os.getenv("DATABASE_URL")
+engine = create_engine(database_url)
 Base = declarative_base(engine)
 
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
