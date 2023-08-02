6d939ac34db6d71135407ab40f4b7430643dc13b
johnmuthua <caveomuthama@gmail.com>
2023-08-03 00:17:39 +0300

    Add cors support

--- server/main.py
+++ server/main.py
@@ -36,11 +36,20 @@ def get_session():
 
 
 app = FastAPI(debug=True)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8080",
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[],
+    allow_headers=["*"],
 )
 
 
