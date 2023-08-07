a729db03a2733409244bcdeae70ad80429fb673b
johnmuthua <caveomuthama@gmail.com>
2023-08-07 10:27:04 +0300

    Add popins

--- client/src/components/hero/Hero.tsx
+++ client/src/components/hero/Hero.tsx
@@ -1,5 +1,41 @@
+import { Box, Button, Typography } from '@mui/material';
+import Work from '../../assets/work.png';
+import './style.scss';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <Box className="hero-wrapper">
+      <Box>
+        <Box>
+          <Typography variant="h4" className="hero-tagline">
+            BEST OPPORTUNITIES FROM AROUND THE GLOBE
+          </Typography>
+          <Typography
+            variant="h4"
+            className="hero-title"
+            sx={{
+              fontWeight: '900',
+              fontSize: '3.5rem',
+            }}
+          >
+            Work and Find Joy Pursing your PASSION
+          </Typography>
+          <Typography variant="caption">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            est voluptas, distinctio et ex nulla mollitia vitae labore beatae!
+            Voluptate doloribus perspiciatis numquam commodi, facilis quod
+            laborum repellat eveniet exercitationem!
+          </Typography>
+        </Box>
+        <Box>
+          <Button variant="contained">Find Out More</Button>
+        </Box>
+      </Box>
+      <Box className="work-wrapper">
+        <img src={Work} alt="Working Enviroment" className="work" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Hero;
