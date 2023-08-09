c75af66802406ebe36a8f060e144bb48215237b0
johnmuthua <caveomuthama@gmail.com>
2023-08-09 20:32:02 +0300

    Add info to steps

--- client/src/components/steps/Steps.tsx
+++ client/src/components/steps/Steps.tsx
@@ -1,11 +1,44 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 const Steps = () => {
   return (
     <Box>
       <Box>
-        <Box></Box>
-        <Box></Box>
+        <Box>
+          <Typography variant="h4">Easy and Fast</Typography>
+          <Typography variant="h5">
+            Fast Track Your Career in 3 Easy Steps
+          </Typography>
+        </Box>
+        <Box>
+          <Box>
+            <Box></Box>
+            <Box>
+              <Typography variant="body2">Browse</Typography>
+              <Typography variant="caption">
+                Search the site on various opportunities that are available
+              </Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Box></Box>
+            <Box>
+              <Typography variant="body2">Apply</Typography>
+              <Typography variant="caption">
+                Apply to the job opportunity that seem most convinient for you
+              </Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Box></Box>
+            <Box>
+              <Typography variant="body2">Register</Typography>
+              <Typography variant="caption">
+                Click on the GET STARTED button to get access to the site
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
       </Box>
       <Box></Box>
     </Box>
