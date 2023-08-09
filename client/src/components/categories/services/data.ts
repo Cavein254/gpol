4d5b4481e2fe62f41e4eaee94f05865cb07e9bb3
johnmuthua <caveomuthama@gmail.com>
2023-08-09 08:28:43 +0300

    Add titile

---
+++ client/src/components/categories/services/data.ts
@@ -0,0 +1,13 @@
+export const services = [
+  {
+    icon: '',
+    title: 'Career Advice',
+    description:
+      "As part of our commitment to helping you succeed, we proudly offer personalized career advice. Our team of experienced professionals is dedicated to assisting you at every stage of your career journey. Whether you're a recent graduate entering the job market or an established professional seeking new horizons, our career advice is tailored to your unique needs.",
+  },
+  {
+    icon: '',
+    title: '',
+    description: '',
+  },
+];
