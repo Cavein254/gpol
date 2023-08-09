ba47a391879ed167b1a03f8f41981e974f9b51e3
johnmuthua <caveomuthama@gmail.com>
2023-08-09 19:54:43 +0300

    Add data

--- client/src/components/sectors/data.ts
+++ client/src/components/sectors/data.ts
@@ -1,7 +1,11 @@
+import Engineering from '../../assets/engineering.jpg';
+import Medicine from '../../assets/medicine.jpg';
+import School from '../../assets/school.jpg';
 export const sector = () => [
   {
     id: 1,
     title: 'Health Sector',
+    src: Medicine,
     rating: 5,
     description:
       "Medicine offers a profound opportunity to make a tangible impact on people's lives by diagnosing, treating, and preventing illnesses, fostering a sense of purpose and fulfillment. ",
@@ -9,6 +13,7 @@ export const sector = () => [
   {
     id: 2,
     title: 'Energy and Construction Sector',
+    src: Engineering,
     rating: 4,
     description:
       'Engineering is a popular career choice due to its ability to transform innovative ideas into practical solutions that shape the world around us. ',
@@ -16,6 +21,7 @@ export const sector = () => [
   {
     id: 3,
     title: 'Educational Sector',
+    src: School,
     rating: 3,
     description:
       "The educational sector is a popular career choice because it provides the chance to shape young minds, inspire future generations, and contribute to society's intellectual growth. ",
