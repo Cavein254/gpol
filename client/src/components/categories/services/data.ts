742a142e276d8bc9294d7340e47ef9d06c8762cc
johnmuthua <caveomuthama@gmail.com>
2023-08-09 09:23:30 +0300

    Add ids

--- client/src/components/categories/services/data.ts
+++ client/src/components/categories/services/data.ts
@@ -1,13 +1,37 @@
 export const services = [
   {
+    id: '1',
     icon: '',
     title: 'Career Advice',
     description:
       "As part of our commitment to helping you succeed, we proudly offer personalized career advice. Our team of experienced professionals is dedicated to assisting you at every stage of your career journey. Whether you're a recent graduate entering the job market or an established professional seeking new horizons, our career advice is tailored to your unique needs.",
   },
   {
+    id: '2',
     icon: '',
-    title: '',
-    description: '',
+    title: 'Trainings',
+    description:
+      "Staying competitive in today's fast-evolving job market often requires continuous skill development. Our advisors will identify the skills that align with your career goals and recommend training options, workshops, and resources to enhance your professional toolkit. We'll guide you toward acquiring the skills that will set you apart in your industry",
+  },
+  {
+    id: '3',
+    icon: '',
+    title: 'Resume Building',
+    description:
+      "Your resume and portfolio are your digital first impressions. Our advisors will help you create compelling resumes, portfolios, and LinkedIn profiles that showcase your achievements, skills, and experience in the best possible light. We'll ensure that your personal brand reflects your unique value proposition and resonates with potential employers.",
+  },
+  {
+    id: '4',
+    icon: '',
+    title: 'Interview Preparation',
+    description:
+      "Ace your interviews with confidence. Our advisors will provide insights into common interview questions, conduct mock interview sessions, and offer feedback to help you refine your interview skills. We'll work together to ensure you're well-prepared to present yourself effectively and make a lasting impression on interviewers.",
+  },
+  {
+    id: '5',
+    icon: '',
+    title: 'Mentorship',
+    description:
+      "Your career journey doesn't stop after landing a job. We're committed to providing ongoing support and mentorship as you navigate your chosen path. Our advisors will be by your side, offering guidance, insights, and encouragement to help you achieve sustained success.",
   },
 ];
