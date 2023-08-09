f407636eeb22d40d7f45da286e28a25f3f58cbd9
johnmuthua <caveomuthama@gmail.com>
2023-08-09 20:12:00 +0300

    items complete

--- client/src/components/sectors/Item.tsx
+++ client/src/components/sectors/Item.tsx
@@ -4,23 +4,18 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-export default function Item() {
+export default function Item({ sector }) {
   return (
     <Card className="item-wrapper">
-      <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
-        title="green iguana"
-      />
+      <CardMedia sx={{ height: 140 }} image={sector.src} title={sector.title} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          Lizard
+          {sector.title}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {sector.description}
         </Typography>
-        <Rating name="read-only" value={5} readOnly />
+        <Rating name="read-only" value={sector.rating} readOnly />
       </CardContent>
     </Card>
   );
