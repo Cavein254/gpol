dfc361363844744ecb483042536b2a022a03debd
johnmuthua <caveomuthama@gmail.com>
2023-08-10 15:22:54 +0300

    completed footer

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -4,14 +4,43 @@ import './styles.scss';
 const Footer = () => {
   return (
     <Box className="footer-container">
-      <Box>
-        <Typography variant="h4">GPOL</Typography>
+      <Box className="footer-links">
+        <Typography
+          sx={{
+            fontFamily: 'Volkhov',
+            fontSize: '2rem',
+            fontWeight: '900',
+          }}
+        >
+          GPOL
+        </Typography>
         <Typography variant="caption">Your Faithful Career Patner</Typography>
       </Box>
-      <Box>
-        <Typography variant="h4">Company</Typography>
+      <Box className="footer-links">
+        <Typography
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: '900',
+            fontSize: '1.2rem',
+          }}
+        >
+          Company
+        </Typography>
         <Typography variant="caption">About</Typography>
         <Typography variant="caption">Career</Typography>
+      </Box>
+      <Box className="footer-links">
+        <Typography
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: '900',
+            fontSize: '1.2rem',
+          }}
+        >
+          Contact
+        </Typography>
+        <Typography variant="caption">Help/FAQs</Typography>
+        <Typography variant="caption">Press</Typography>
       </Box>
       <Box></Box>
     </Box>
