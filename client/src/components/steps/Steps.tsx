8768f57bb06346533423a5cbf6a223f0c4af1f9f
johnmuthua <caveomuthama@gmail.com>
2023-08-10 15:48:00 +0300

    completed more things

--- client/src/components/steps/Steps.tsx
+++ client/src/components/steps/Steps.tsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from '@mui/material';
 import { stepData } from './data';
+import './styles.scss';
 const Steps = () => {
   const step = stepData.map((item) => {
     return (
-      <Box key={item.id}>
+      <Box key={item.id} className="step-container">
         <Box>
-          <item.icon />
+          <item.icon className="step-icon" />
         </Box>
         <Box>
           <Typography
@@ -39,6 +40,32 @@ const Steps = () => {
         margin: '1rem',
       }}
     >
+      <Box>
+        <Typography
+          variant="h4"
+          sx={{
+            fontFamily: 'Poppins',
+            fontWeight: '900',
+            fontSize: '0.9rem',
+            color: '#df6951',
+          }}
+        >
+          Easy and fast
+        </Typography>
+        <Typography
+          variant="h5"
+          sx={{
+            paddingTop: '1.2rem',
+            fontFamily: 'Volkhov',
+            lineHeight: '0.95',
+            fontWeight: '900',
+            fontSize: '3.5rem',
+            color: '#181e4b',
+          }}
+        >
+          Jump Start your next Career in 3 easy steps
+        </Typography>
+      </Box>
       <Box>{step}</Box>
     </Box>
   );
