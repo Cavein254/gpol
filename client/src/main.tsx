16ec3745bc5d5f6d7c48513085fef235da714ca8
johnmuthua <caveomuthama@gmail.com>
2023-08-11 19:49:24 +0300

    Add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App.tsx';
 import { store } from './App/store';
+import Header from './components/header/Header.tsx';
 import './index.css';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <div>Hello world!</div>,
-  },
-]);
+import Main from './scenes/Main.tsx';
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Root />}>
+      <Route path="/" element={<Main />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
