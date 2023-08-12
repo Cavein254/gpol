699835f16f4b6ce4c17e3b3346f44b4eef711da7
johnmuthua <caveomuthama@gmail.com>
2023-08-12 16:57:12 +0300

    Add data

---
+++ client/src/Admin/left/data.ts
@@ -0,0 +1,17 @@
+export const JobStatsData = [
+  {
+    id: 1,
+    text: 'Active Jobs',
+    stats: 12,
+  },
+  {
+    id: 2,
+    text: 'Completed Jobs',
+    stats: 50,
+  },
+  {
+    id: 3,
+    text: 'Total Jobs',
+    stats: 210,
+  },
+];
