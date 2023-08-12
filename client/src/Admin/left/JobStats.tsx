4b5fb77e0e563e93bfe530f08d9d79aed35391cf
johnmuthua <caveomuthama@gmail.com>
2023-08-12 17:02:28 +0300

    add data

--- client/src/Admin/left/JobStats.tsx
+++ client/src/Admin/left/JobStats.tsx
@@ -1,42 +1,45 @@
 import { Box, Typography } from '@mui/material';
-import ActiveJobs from '../../assets/activeJobs.svg';
+import { JobStatsData } from './data';
 const JobStats = () => {
-  return (
-    <Box className="jobstats-container">
-      <Box
-        className="job-item"
-        sx={{
-          boxShadow: 3,
-        }}
-      >
-        <Box className="item-left">
-          <Typography
-            variant="h4"
-            sx={{
-              fontFamily: 'Poppins',
-              fontWeight: 400,
-              fontSize: '1rem',
-            }}
-          >
-            Active Jobs
-          </Typography>
-          <Typography
-            variant="h4"
-            sx={{
-              fontFamily: 'Poppins',
-              fontWeight: 900,
-              fontSize: '2.5rem',
-            }}
-          >
-            12
-          </Typography>
-        </Box>
-        <Box className="item-left-image">
-          <img src={ActiveJobs} alt="jobs" />
+  const jobs = JobStatsData.map((job) => {
+    return (
+      <Box className="jobstats-container" key={job.id}>
+        <Box
+          className="job-item"
+          sx={{
+            boxShadow: 3,
+          }}
+        >
+          <Box className="item-left">
+            <Typography
+              variant="h4"
+              sx={{
+                fontFamily: 'Poppins',
+                fontWeight: 400,
+                fontSize: '1rem',
+              }}
+            >
+              {job.text}
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{
+                fontFamily: 'Poppins',
+                fontWeight: 900,
+                fontSize: '2.5rem',
+              }}
+            >
+              {job.stats}
+            </Typography>
+          </Box>
+          <Box className="item-left-image">
+            <img src={job.icon} alt="jobs" />
+          </Box>
         </Box>
       </Box>
-    </Box>
-  );
+    );
+  });
+  return { jobs };
 };
 
 export default JobStats;
--- client/src/Admin/left/data.ts
+++ client/src/Admin/left/data.ts
@@ -1,17 +1,24 @@
+import ActiveJobs from '../../assets/activeJobs.svg';
+import CompletedJobs from '../../assets/completedjobs.svg';
+import TotalJobs from '../../assets/totalJobs.svg';
+
 export const JobStatsData = [
   {
     id: 1,
     text: 'Active Jobs',
     stats: 12,
+    icon: ActiveJobs,
   },
   {
     id: 2,
     text: 'Completed Jobs',
     stats: 50,
+    icon: CompletedJobs,
   },
   {
     id: 3,
     text: 'Total Jobs',
     stats: 210,
+    icon: TotalJobs,
   },
 ];
