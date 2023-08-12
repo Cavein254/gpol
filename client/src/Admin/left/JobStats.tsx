19804e6f6039cf4de9f9372594ac0bcfa2716d27
johnmuthua <caveomuthama@gmail.com>
2023-08-12 16:54:29 +0300

    Add job stats

--- client/src/Admin/left/JobStats.tsx
+++ client/src/Admin/left/JobStats.tsx
@@ -1,5 +1,42 @@
+import { Box, Typography } from '@mui/material';
+import ActiveJobs from '../../assets/activeJobs.svg';
 const JobStats = () => {
-  return <div>Job Stats</div>;
+  return (
+    <Box className="jobstats-container">
+      <Box
+        className="job-item"
+        sx={{
+          boxShadow: 3,
+        }}
+      >
+        <Box className="item-left">
+          <Typography
+            variant="h4"
+            sx={{
+              fontFamily: 'Poppins',
+              fontWeight: 400,
+              fontSize: '1rem',
+            }}
+          >
+            Active Jobs
+          </Typography>
+          <Typography
+            variant="h4"
+            sx={{
+              fontFamily: 'Poppins',
+              fontWeight: 900,
+              fontSize: '2.5rem',
+            }}
+          >
+            12
+          </Typography>
+        </Box>
+        <Box className="item-left-image">
+          <img src={ActiveJobs} alt="jobs" />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default JobStats;
