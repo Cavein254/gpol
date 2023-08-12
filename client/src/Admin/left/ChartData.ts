56aa4ff276da28df9ba02a82b4723c024f7fc285
johnmuthua <caveomuthama@gmail.com>
2023-08-12 17:33:25 +0300

    Add responsive line

---
+++ client/src/Admin/left/ChartData.ts
@@ -0,0 +1,272 @@
+export const ChartData = [
+  {
+    id: 'japan',
+    color: 'hsl(89, 70%, 50%)',
+    data: [
+      {
+        x: 'plane',
+        y: 118,
+      },
+      {
+        x: 'helicopter',
+        y: 217,
+      },
+      {
+        x: 'boat',
+        y: 41,
+      },
+      {
+        x: 'train',
+        y: 205,
+      },
+      {
+        x: 'subway',
+        y: 128,
+      },
+      {
+        x: 'bus',
+        y: 176,
+      },
+      {
+        x: 'car',
+        y: 220,
+      },
+      {
+        x: 'moto',
+        y: 58,
+      },
+      {
+        x: 'bicycle',
+        y: 149,
+      },
+      {
+        x: 'horse',
+        y: 269,
+      },
+      {
+        x: 'skateboard',
+        y: 261,
+      },
+      {
+        x: 'others',
+        y: 252,
+      },
+    ],
+  },
+  {
+    id: 'france',
+    color: 'hsl(147, 70%, 50%)',
+    data: [
+      {
+        x: 'plane',
+        y: 18,
+      },
+      {
+        x: 'helicopter',
+        y: 17,
+      },
+      {
+        x: 'boat',
+        y: 104,
+      },
+      {
+        x: 'train',
+        y: 71,
+      },
+      {
+        x: 'subway',
+        y: 290,
+      },
+      {
+        x: 'bus',
+        y: 231,
+      },
+      {
+        x: 'car',
+        y: 287,
+      },
+      {
+        x: 'moto',
+        y: 235,
+      },
+      {
+        x: 'bicycle',
+        y: 223,
+      },
+      {
+        x: 'horse',
+        y: 297,
+      },
+      {
+        x: 'skateboard',
+        y: 190,
+      },
+      {
+        x: 'others',
+        y: 5,
+      },
+    ],
+  },
+  {
+    id: 'us',
+    color: 'hsl(51, 70%, 50%)',
+    data: [
+      {
+        x: 'plane',
+        y: 12,
+      },
+      {
+        x: 'helicopter',
+        y: 178,
+      },
+      {
+        x: 'boat',
+        y: 37,
+      },
+      {
+        x: 'train',
+        y: 125,
+      },
+      {
+        x: 'subway',
+        y: 4,
+      },
+      {
+        x: 'bus',
+        y: 269,
+      },
+      {
+        x: 'car',
+        y: 90,
+      },
+      {
+        x: 'moto',
+        y: 201,
+      },
+      {
+        x: 'bicycle',
+        y: 119,
+      },
+      {
+        x: 'horse',
+        y: 257,
+      },
+      {
+        x: 'skateboard',
+        y: 206,
+      },
+      {
+        x: 'others',
+        y: 118,
+      },
+    ],
+  },
+  {
+    id: 'germany',
+    color: 'hsl(36, 70%, 50%)',
+    data: [
+      {
+        x: 'plane',
+        y: 60,
+      },
+      {
+        x: 'helicopter',
+        y: 283,
+      },
+      {
+        x: 'boat',
+        y: 124,
+      },
+      {
+        x: 'train',
+        y: 29,
+      },
+      {
+        x: 'subway',
+        y: 146,
+      },
+      {
+        x: 'bus',
+        y: 212,
+      },
+      {
+        x: 'car',
+        y: 223,
+      },
+      {
+        x: 'moto',
+        y: 41,
+      },
+      {
+        x: 'bicycle',
+        y: 51,
+      },
+      {
+        x: 'horse',
+        y: 46,
+      },
+      {
+        x: 'skateboard',
+        y: 229,
+      },
+      {
+        x: 'others',
+        y: 76,
+      },
+    ],
+  },
+  {
+    id: 'norway',
+    color: 'hsl(138, 70%, 50%)',
+    data: [
+      {
+        x: 'plane',
+        y: 177,
+      },
+      {
+        x: 'helicopter',
+        y: 59,
+      },
+      {
+        x: 'boat',
+        y: 277,
+      },
+      {
+        x: 'train',
+        y: 196,
+      },
+      {
+        x: 'subway',
+        y: 190,
+      },
+      {
+        x: 'bus',
+        y: 127,
+      },
+      {
+        x: 'car',
+        y: 133,
+      },
+      {
+        x: 'moto',
+        y: 278,
+      },
+      {
+        x: 'bicycle',
+        y: 97,
+      },
+      {
+        x: 'horse',
+        y: 296,
+      },
+      {
+        x: 'skateboard',
+        y: 11,
+      },
+      {
+        x: 'others',
+        y: 103,
+      },
+    ],
+  },
+];
